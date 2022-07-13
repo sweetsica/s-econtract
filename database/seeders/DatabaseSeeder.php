ed0c16e65564330b045cea9d6a413609aa18d215
sweetsica <barondeepsnow@gmail.com>
2022-07-13 22:38:38 +0700

    fix migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LocationSeeder::class,
             DepartmentSeeder::class,
-            DoppelherzSign::class
+            DoppelherzSignSeeder::class
         ]);
     }
 }
