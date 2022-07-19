deb470250ffb53e66ad75330dd8cbbff5d18459d
sweetsica <barondeepsnow@gmail.com>
2022-07-19 15:53:42 +0700

    fix store signature

--- app/Http/Controllers/SignatureController.php
+++ app/Http/Controllers/SignatureController.php
@@ -23,25 +23,13 @@ class SignatureController extends Controller
         return view('back-end.signature.signature');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-
     public function store(Request $request)
     {
         try{
             $id_contract = Session::get('id_contract');
             $contract = Contract::where('id', '=', $id_contract)->first();
-            $url_image = $this->save_sign($request, sprintf("%s-%s", $contract['id'], $contract["account_phone"]));
+            $url_image = $this->save_sign($request, sprintf("%s-%s", $contract['id'], $contract["store_phone"]));
             $contract['store_sign_img'] = $url_image;
-            $contract['store_sign_img'] = Session::get('url_true');
             $contract['name_doppelherz'] = $request['name_doppelherz'];
             $contract['bank_doppelherz'] = $request['bank_doppelherz'];
             $doppelherz_image = DoppelherzSign::where('name', '=', $contract['store_sign_img_doppelherz'])->get()->first();
@@ -53,51 +41,5 @@ class SignatureController extends Controller
             return redirect()->route('contract.return.export.signed');
         }
 
-    }
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
