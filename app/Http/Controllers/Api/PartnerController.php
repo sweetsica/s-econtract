0bfc0de2ca2428ed57617a3444b08fe9d2e4208f
sweetsica <barondeepsnow@gmail.com>
2022-08-03 11:14:59 +0700

    a

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -50,8 +50,11 @@ class PartnerController extends Controller
                 $checkPartner = Partner::where('owner_phone', $request->owner_phone)->orWhere('owner_id_numb', $request->owner_id_numb)->first();
                 if ($checkPartner) {
                     $data = $checkPartner;
-                    $data['notice'] = 'Bạn đã là đối tác!';
-                    return response($data, 200);
+                    return response()->json([
+                        'data' => $data,
+                        'notice' => 'Bạn đã là đối tác!',
+                        'redirect_url' => url('/doi-tac/dang-nhap')
+                    ], 200);
                 } else {
                     //Nếu thông tin partner chưa có
                     $partner = Partner::create($request->only(['owner_name', 'owner_id_numb', 'owner_id_numb_created_at', 'owner_id_numb_created_locate', 'owner_sex', 'owner_dob', 'owner_age', 'owner_token', 'owner_phone', 'owner_email', 'owner_mst']));
@@ -60,7 +63,11 @@ class PartnerController extends Controller
                     $contract->contract_code = 'HD-2022/' . $contract->id . $contract->created_at->format('-His');
                     $contract->partnerId = $partner->id;
                     $contract->save();
-                    return response()->json($contract,200);
+                    return response()->json([
+                        'data' => $contract,
+                        'notice' => 'Thêm hợp đồng thành công!',
+                        'redirect_url' => url('/doi-tac/dang-nhap')
+                    ], 200);
                 }
             } else {
                 //Đăng ký đầy đủ
