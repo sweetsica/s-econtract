adff7bad8c8fb139c272e7e041aae11e295a94d9
sweetsica <barondeepsnow@gmail.com>
2022-08-03 10:14:03 +0700

    dcm deo duoc nua bo may nghi

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -47,12 +47,11 @@ class PartnerController extends Controller
 
             if ($request['contract_mode'] == 0) {
                 //Đăng ký nhanh
-//                echo 'Tạo nhanh';
                 $checkPartner = Partner::where('owner_phone', $request->owner_phone)->orWhere('owner_id_numb', $request->owner_id_numb)->first();
                 if ($checkPartner) {
                     $data = $checkPartner;
                     $data['notice'] = 'Bạn đã là đối tác!';
-                    return response($data, 200)->header("Access-Control-Allow-Origin",  "*");
+                    return response($data, 200);
                 } else {
                     //Nếu thông tin partner chưa có
                     $partner = Partner::create($request->only(['owner_name', 'owner_id_numb', 'owner_id_numb_created_at', 'owner_id_numb_created_locate', 'owner_sex', 'owner_dob', 'owner_age', 'owner_token', 'owner_phone', 'owner_email', 'owner_mst']));
@@ -61,28 +60,22 @@ class PartnerController extends Controller
                     $contract->contract_code = 'HD-2022/' . $contract->id . $contract->created_at->format('-His');
                     $contract->partnerId = $partner->id;
                     $contract->save();
-                    return response()->json($contract,200)->header("Access-Control-Allow-Origin",  "*");
+                    return response()->json($contract,200);
                 }
             } else {
                 //Đăng ký đầy đủ
-//                echo 'Tạo chậm';
                 $checkPartner = Partner::where('owner_phone', $request->owner_phone)->orWhere('owner_id_numb', $request->owner_id_numb)->first();
                 if ($checkPartner) {
-//                    echo 'Bạn đã là đối tác!';
-                    $partner = Partner::find($checkPartner->id)->update($request->only(['owner_sex','owner_dob','owner_age','owner_id_numb_created_at','owner_id_numb_created_locate','owner_mst']));
-//                    $checkPartner = Partner::update($request->only(['owner_sex','owner_dob','owner_age','owner_id_numb_created_at','owner_id_numb_created_locate','owner_mst']));
-//                    $partner->save();
                     $contract = Contract::create($request->only(['store_contract_type', 'store_name', 'store_add_DKKD', 'store_local_DKKD', 'store_add_GH', 'store_local_GH', 'store_headman', 'store_mst', 'member_id', 'store_phone', 'store_website', 'store_GPDKKD', 'store_id_Numb_GPDKKD', 'store_bank', 'store_bank_holder', 'store_bank_numb', 'store_contact_name', 'store_contact_phone', 'store_contact_position', 'store_effect', 'store_started', 'store_end', 'contract_level','contract_mode', 'store_signed', 'store_sign_img', 'store_sign_img_doppelherz', 'store_token']));
                     $contract->contract_mode = $request->contract_mode;
                     $contract->contract_code = 'HD-2022/' . $contract->id . $contract->created_at->format('-His');
                     $contract->partnerId = $checkPartner->id;
                     $contract->save();
-//                    echo 'Thêm hợp đồng và cập nhật đối tác thành công';
                     return response()->json([
                         'data' => $contract,
                         'notice' => 'Tạo thành công, chúng tôi sẽ liên hệ bạn sớm nhất!',
                         'redirect_url' => url('/doi-tac/dang-nhap')
-                    ], 200)->header("Access-Control-Allow-Origin",  "*");
+                    ], 200);
                 } else {
                     //Nếu thông tin partner chưa có
                     $partner = Partner::create($request->only(['owner_name', 'owner_id_numb', 'owner_id_numb_created_at', 'owner_id_numb_created_locate', 'owner_sex', 'owner_dob', 'owner_age', 'owner_token', 'owner_phone', 'owner_email', 'owner_mst']));
@@ -95,14 +88,14 @@ class PartnerController extends Controller
                         'data' => $contract,
                         'notice' => 'Tạo thành công, chúng tôi sẽ liên hệ bạn sớm nhất!',
                         'redirect_url' => url('/doi-tac/dang-nhap')
-                    ], 200)->header("Access-Control-Allow-Origin",  "*");
+                    ], 200);
                 }
             }
             return response()->json([
                 'data' => $checkPartne,
                 'notice' => 'Tạo thành công, chúng tôi sẽ liên hệ bạn sớm nhất!',
                 'redirect_url' => url('/doi-tac/dang-nhap')
-            ], 200)->header("Access-Control-Allow-Origin",  "*");
+            ], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
         }
